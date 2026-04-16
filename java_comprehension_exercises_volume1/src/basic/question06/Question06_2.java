f1d7d01079463bb8266edf8de00f2be2f5923047
sminemu <sminemu@C010423630.zipangu.cac.co.jp>
2026-04-16 14:27:02 +0900

    basic完了

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
@@ -6,7 +6,12 @@ public class Question06_2 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
-		
+		String[] studentName = { "かなこ", "ゆうた", "あい", "はるひ", "かずき" };
+
+		for (String str : studentName) {
+			System.out.println(str);
+
+		}
+
 	}
 }
--- java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_4.java
@@ -1,12 +1,36 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_4 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		// TODO:ここに実装
+		//本の冊数
+		int amountBooks = 0;
+		//本を売却するかどうか
+		int sellFlag = 0;
+		//本の値段
+		int bookPrice = 200;
+
+		System.out.println("本を売却します。");
+
+		do {
+			//冊数を増やす
+			amountBooks++;
+			//メッセージ表示
+			System.out.println("売却する本の冊数を1つ増やしますか？");
+			System.out.print("はい：０、いいえ：１ >");
+			//入力
+			String choice = reader.readLine();
+			sellFlag = Integer.parseInt(choice);
+
+		} while (sellFlag == 0);
+		System.out.println("売却する本の冊数は" + amountBooks + "冊です。");
+		System.out.println("売却した本の合計金額は" + (amountBooks * bookPrice) + "円です。");
 
 	}
 
